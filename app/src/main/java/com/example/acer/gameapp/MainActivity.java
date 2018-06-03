2f2b125f933846c8519adb449bbbdd7a9ac2b176
VenomRishi <30369897+VenomRishi@users.noreply.github.com>
2018-06-03 15:29:34 +0530

    Initial commit

---
+++ app/src/main/java/com/example/acer/gameapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.acer.gameapp;
+
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        this.setContentView(new GameSurface(this));
+    }
+
+}
